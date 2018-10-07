bc3c642082362a5dc3e2ea46ed1e31127c2e002f
Miles Hill <miles1618@gmail.com>
2018-10-06 20:30:38 -0500

    Control button

--- src/App.js
+++ src/App.js
@@ -1,32 +1,51 @@
 import React, { Component } from 'react';
 import './App.css';
 import styled from 'styled-components';
-import div from 'styled-components';
+import css from 'styled-components';
 
 const Logo = styled.div`
   font-size: 1.5em;
   `;
 
 const ControlButton = styled.div`
+  ${props => props.active && `
+    text-shadow: 0px 0px 60px #03ff03;
+  `}
 `;
 
+
 const AppLayout = styled.div`
-  display: grid;
-  grid-template-columns: 100px  auto 100px 100px;
   padding: 40px;
 `;
 
+const Bar = styled.div`
+  display: grid;
+  grid-template-columns: 100px  auto 100px 100px;
+  margin-bottom: 40px;
+`;
+
+const Content = styled.div`
+`;
 
 
 
 class App extends Component {
+  state = {
+    page: 'dashboard' 
+  };
+
+  displayingDashboard = () => this.state.page === 'dashboard';
+  displayingSettings = () => this.state.page === 'settings';
   render() {
     return (
       <AppLayout>
+      <Bar>
         <Logo>CryptoDash</Logo>
         <div></div>
-        <ControlButton>Dashboard</ControlButton>
-        <ControlButton>Settings</ControlButton>
+        <ControlButton active={this.displayingDashboard()}>Dashboard</ControlButton>
+        <ControlButton active={this.displayingSettings()}>Settings</ControlButton>
+      </Bar>
+      <Content>Hello {this.state.page}</Content>
       </AppLayout>
     );
   }
