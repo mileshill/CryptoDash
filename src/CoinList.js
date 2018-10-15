4101228779b12663225264e19a6896968e60a650
Miles Hill <miles1618@gmail.com>
2018-10-15 09:15:25 -0500

    Coin favorites grid; five default

--- src/CoinList.js
+++ src/CoinList.js
@@ -5,7 +5,10 @@ import { subtleBoxShadow, lightBlueBackground, greenBoxShadow, redBoxShadow } fr
 
 const CoinGrid = styled.div`
     display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+    grid-template-columns: repeat(5, 1fr);
+    ${props => props.count && css`
+        grid-template-columns: repeat(${props.count > 5 ? props.count : 5}, 1fr);
+    `}
     grid-gap: 15px;
     margin-top: 40px;
 `;
@@ -50,9 +53,9 @@ const DeleteIcon = styled.div`
 export default function(favorites=false) {
     let coinKeys = favorites ? 
         this.state.favorites : 
-        (this.state.filteredCoins && Object.keys(this.state.filteredCoins) || Object.keys(this.state.coinList).slice(0, 50));
+        ((this.state.filteredCoins && Object.keys(this.state.filteredCoins)) || Object.keys(this.state.coinList).slice(0, 50));
     return (
-        <CoinGrid>
+        <CoinGrid count={favorites && this.state.favorites.length}>
         {
             coinKeys.map((coinKey, idx) => 
                 <CoinTile 
